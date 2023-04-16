87e51022348531f8895c65abd68f1c0026de3df3
Nelly Kim <sk8high3r@ya.ru>
2023-04-16 19:29:17 +0300

    Correct Rubocop offences

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :task do
-    name { "MyString" }
-    description { "MyText" }
+    name { 'MyString' }
+    description { 'MyText' }
     author factory: :manager
     assignee factory: :developer
-    state { "MyString" }
-    expired_at { "2023-04-16" }
+    state { 'MyString' }
+    expired_at { '2023-04-16' }
   end
 end
