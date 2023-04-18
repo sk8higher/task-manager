500edb4f45611655c16677ea9ccaa9af0aa32335
Nelly Kim <sk8high3r@ya.ru>
2023-04-18 21:34:59 +0300

    Remove repetition from state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,28 +8,20 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 500 }
 
   state_machine :state, initial: :new_task do
-    event :start_development do
-      transition new_task: :in_development
+    event :to_development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
 
-    event :start_qa do
+    event :to_qa do
       transition in_development: :in_qa
     end
 
-    event :return_tested_to_development do
-      transition in_qa: :in_development
-    end
-
-    event :start_code_review do
+    event :to_code_review do
       transition in_qa: :in_code_review
     end
 
-    event :mark_ready_for_release do
+    event :to_ready_for_release do
       transition in_code_review: :ready_for_release
-    end
-
-    event :return_reviewed_to_development do
-      transition in_code_review: :in_development
     end
 
     event :release do
