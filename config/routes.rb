aa28a27be014dd612db233bb9710762ac322963f
Nelly Kim <sk8high3r@ya.ru>
2023-05-15 17:22:47 +0300

    Add routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  namespace :web do
-    get 'sessions/new'
+  root :to => 'web/boards#show'
+
+  scope module: :web do
+    resource :board, only: :show
+    resource :session, only: :new
   end
-  namespace :web do
-    get 'boards/show'
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
