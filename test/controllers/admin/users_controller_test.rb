40837e32dcadda71305a3057207228813d2f7db9
Nelly Simkova <sk8high3r@ya.ru>
2023-06-17 21:26:35 +0300

    Run Rubocop

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 include AuthHelper
 
 class Admin::UsersControllerTest < ActionController::TestCase
-  test "should get show" do
+  test 'should get show' do
     user = create(:user)
     get :show, params: { id: user.id }
     assert_response :success
