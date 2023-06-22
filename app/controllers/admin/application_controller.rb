4eb7d0d1e30c2ebcd73ad2fa3bceaf613de16ff3
Nelly Simkova <sk8high3r@ya.ru>
2023-06-22 14:31:09 +0300

    Remove auth user from Admin controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,8 +2,6 @@ class Admin::ApplicationController < ApplicationController
   include AuthHelper
   helper_method :current_user
 
-  before_action :authenticate_user!, :authorize
-
   def authorize
     render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
   end
