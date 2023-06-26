63ee1c1a3865ad87c9efafef2c99037ce0d2a817
Nelly Simkova <sk8high3r@ya.ru>
2023-06-26 17:01:46 +0300

    Run Rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,7 @@ class Task < ApplicationRecord
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
 
-  def self.ransackable_attributes(auth_object = nil)
+  def self.ransackable_attributes(_auth_object = nil)
     ['assignee_id', 'author_id', 'created_at', 'description', 'expired_at', 'id', 'name', 'state', 'updated_at']
   end
 
