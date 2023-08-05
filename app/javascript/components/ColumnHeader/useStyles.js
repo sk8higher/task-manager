f763ec659b927b439b7d7175102088d0f9cf047c
Nelly Simkova <sk8high3r@ya.ru>
2023-08-05 17:38:36 +0300

    Fix column styles

--- app/javascript/components/ColumnHeader/useStyles.js
+++ app/javascript/components/ColumnHeader/useStyles.js
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
+    width: 250,
     height: 42,
     display: 'flex',
     alignItems: 'center',
