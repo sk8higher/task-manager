f6deeff4240486aecf6374f86c71b96d3ba7bd21
Nelly Simkova <sk8high3r@ya.ru>
2023-08-12 19:29:11 +0300

    Fix Users tests

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionController::TestCase
+  setup do
+    user = create(:user)
+    sign_in(user)
+  end
+
   test 'should get show' do
     user = create :user
     get :show, params: { id: user.id, format: :json }
