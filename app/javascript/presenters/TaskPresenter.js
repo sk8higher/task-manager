ded64254251df9465ba7edb6fbac73ab0db8ce82
Nelly Simkova <sk8high3r@ya.ru>
2023-10-26 13:15:40 +0300

    Move states to TaskPresenter

--- app/javascript/presenters/TaskPresenter.js
+++ app/javascript/presenters/TaskPresenter.js
@@ -3,6 +3,16 @@ import PropTypesPresenter from 'utils/PropTypesPresenter';
 
 import UserPresenter from './UserPresenter';
 
+export const STATES = [
+  { key: 'new_task', value: 'New' },
+  { key: 'in_development', value: 'In Dev' },
+  { key: 'in_qa', value: 'In QA' },
+  { key: 'in_code_review', value: 'In CR' },
+  { key: 'ready_for_release', value: 'Ready for release' },
+  { key: 'released', value: 'Released' },
+  { key: 'archived', value: 'Archived' },
+];
+
 export default new PropTypesPresenter(
   {
     id: PropTypes.number,
@@ -39,13 +49,3 @@ export default new PropTypesPresenter(
     },
   },
 );
-
-export const STATES = [
-  { key: 'new_task', value: 'New' },
-  { key: 'in_development', value: 'In Dev' },
-  { key: 'in_qa', value: 'In QA' },
-  { key: 'in_code_review', value: 'In CR' },
-  { key: 'ready_for_release', value: 'Ready for release' },
-  { key: 'released', value: 'Released' },
-  { key: 'archived', value: 'Archived' },
-];
