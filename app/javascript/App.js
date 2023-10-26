9f1d1b3866cde8337c0bb6e4a426f4fb7a61fe66
Nelly Simkova <sk8high3r@ya.ru>
2023-10-26 13:13:50 +0300

    Remove MUITheme (doesn't work)

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -3,14 +3,11 @@ import React from 'react';
 import store from 'store';
 import { Provider } from 'react-redux';
 import TaskBoard from 'containers/TaskBoard';
-import MUITheme from 'MUITheme/MUITheme';
 
 function App() {
   return (
     <Provider store={store}>
-      <MUITheme>
-        <TaskBoard />
-      </MUITheme>
+      <TaskBoard />
     </Provider>
   );
 }
