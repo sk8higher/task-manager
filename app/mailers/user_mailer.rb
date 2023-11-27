d4e52a251654d6d8a08a3b7ac75263dc6e653f29
Nelly Simkova <sk8high3r@ya.ru>
2023-11-27 16:48:40 +0300

    Add mailer task_created action with test and view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  default from: 'noreply@taskmanager.com'
+
+  def task_created
+    user = params[:user]
+    @task = params[:task]
+
+    mail(to: user.email, subject: 'New Task Created')
+  end
 end
