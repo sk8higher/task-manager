cebd2d6bcae439d3b42a968cd2cf8840aae9471e
Nelly Simkova <sk8high3r@ya.ru>
2023-11-28 19:19:59 +0300

    Add task_destroyed and task_updated methods

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -16,4 +16,36 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal 'New Task Created', email.subject
     assert email.body.to_s.include?("Task #{task.id} was created")
   end
+
+  test 'task updated' do
+    user = create(:user)
+    task = create(:task)
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_updated
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Task Was Updated', email.subject
+    assert email.body.to_s.include?("Task #{task.id} was updated")
+  end
+
+  test 'task destroyed' do
+    user = create(:user)
+    task = create(:task)
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_destroyed
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Task Was Destroyed', email.subject
+    assert email.body.to_s.include?("Task #{task.id} was destroyed")
+  end
 end
