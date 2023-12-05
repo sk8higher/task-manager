7d13eb64f20396d8c5bd2d534fffc22fa9e2e26a
Nelly Simkova <sk8high3r@ya.ru>
2023-12-05 14:54:32 +0300

    Add mailer preview

--- test/mailers/previews/password_mailer_preview.rb
+++ test/mailers/previews/password_mailer_preview.rb
@@ -3,7 +3,11 @@ class PasswordMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/password_mailer/reset
   def reset
-    PasswordMailer.reset
-  end
+    user = User.first
+    token = user.generate_password_token!
 
+    params = { user: user, token: token }
+
+    PasswordMailer.with(params).reset
+  end
 end
