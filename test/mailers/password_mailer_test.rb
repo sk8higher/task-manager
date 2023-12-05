d49681e438b227958bae5510d294d20e1d78bf3c
Nelly Simkova <sk8high3r@ya.ru>
2023-12-05 15:06:00 +0300

    Fix UserMailer tests and write PasswordMailer test

--- test/mailers/password_mailer_test.rb
+++ test/mailers/password_mailer_test.rb
@@ -2,11 +2,18 @@ require "test_helper"
 
 class PasswordMailerTest < ActionMailer::TestCase
   test "reset" do
-    mail = PasswordMailer.reset
-    assert_equal "Reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+    user = create(:user)
+    token = user.generate_password_token!
+    params = { user: user, token: token }
+    email = PasswordMailer.with(params).reset
 
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal "Change your password", email.subject
+    assert email.body.to_s.include?("someone requested a reset of your password")
+  end
 end
