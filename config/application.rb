e57211ffbd4e09721ccb98f94eeaacc8f6116574
Nelly Simkova <sk8high3r@ya.ru>
2023-12-21 12:25:24 +0300

    Remove services folder from autoload

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module TaskManager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoload_paths << "#{Rails.root}/services"
   end
 end
