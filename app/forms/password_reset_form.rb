16dd21d92f2b8ed2fe2d8877d22c591b5232b071
Nelly Simkova <sk8high3r@ya.ru>
2024-03-05 19:55:21 +0300

    Add forms for password reset\set

---
+++ app/forms/password_reset_form.rb
@@ -0,0 +1,18 @@
+class PasswordResetForm
+  include ActiveModel::Model
+
+  attr_accessor :email
+
+  validates :email, presence: true, format: { with: /\A(.+)@(.+)\z/ }
+  validate :user_valid?
+
+  def user
+    @user ||= User.find_by_email(email)
+  end
+
+  private
+
+  def user_valid?
+    errors.add(:email, :email_not_found) if user.blank?
+  end
+end
---
+++ app/forms/password_set_form.rb
@@ -0,0 +1,11 @@
+class PasswordSetForm
+  include ActiveModel::Model
+
+  attr_accessor(
+    :password,
+    :password_confirmation,
+  )
+
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+end
