a04b00257620856fb33ff59de6731053b3efad19
Nelly Simkova <sk8high3r@ya.ru>
2024-03-06 14:19:40 +0300

    Fix token generation

--- app/services/password_resets_service.rb
+++ app/services/password_resets_service.rb
@@ -2,15 +2,15 @@ module PasswordResetsService
   class << self
     def generate_password_token!(user)
       user.update!({
-                     password_reset_token: generate_token,
-                     password_reset_token_expires_at: Time.now.utc,
+                     password_reset_token: SecureRandom.hex(20),
+                     password_reset_token_expires_at: Time.now.utc + 24.hours,
                    })
 
       user.password_reset_token
     end
 
     def password_token_valid?(user)
-      (user&.password_reset_token_expires_at + 24.hours) > Time.now.utc
+      user&.password_reset_token_expires_at > Time.now.utc
     end
 
     def change_password!(user, password)
@@ -19,12 +19,6 @@ module PasswordResetsService
                      password_reset_token_expires_at: nil,
                      password: password,
                    })
-    end
-
-    private
-
-    def generate_token
-      SecureRandom.hex(20)
     end
   end
 end
