07f51921f0364466cb70a600c24e21473282040a
Nelly Simkova <sk8high3r@ya.ru>
2024-03-06 16:35:26 +0300

    Remove comments

--- test/mailers/previews/password_mailer_preview.rb
+++ test/mailers/previews/password_mailer_preview.rb
@@ -1,6 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/password_mailer
 class PasswordMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/password_mailer/reset
   def reset
     user = User.first
     token = user.generate_password_token!
