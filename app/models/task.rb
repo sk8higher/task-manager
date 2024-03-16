47f9f51029d0ecc031eda9dcf8388a648113f00c
Nelly Simkova <sk8high3r@ya.ru>
2024-03-16 23:36:04 +0300

    Add actions to controller, service, form and routes

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,8 @@ class Task < ApplicationRecord
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
 
+  has_one_attached :image
+
   def self.ransackable_attributes(_auth_object = nil)
     ['assignee_id', 'author_id', 'created_at', 'description', 'expired_at', 'id', 'name', 'state', 'updated_at']
   end
